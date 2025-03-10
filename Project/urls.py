d17fd5a579c6b489884ba9a07953d2a349151a00
Shrey2892 <nairshreya227@gmail.com>
2025-03-10 12:10:52 +0530

    Task-2

--- Project/urls.py
+++ Project/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('google_auth.urls')),
+    path('/', include('google_auth.urls')),
     path('drive/', include('drive.urls')),
     path('chat/', include('chat.urls')),
 ]
