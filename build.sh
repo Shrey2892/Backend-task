828b6ca4e99c8c3c3e8aa42a441a1c8f32533057
Shrey2892 <nairshreya227@gmail.com>
2025-03-11 13:22:33 +0530

    Deleted Files

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Ensure the correct Python version is used (Render should pick from runtime.txt)
-python --version
-
-# Install dependencies
-pip install --no-cache-dir --force-reinstall -r requirements.txt
